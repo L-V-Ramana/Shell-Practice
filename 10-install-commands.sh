437df01ca54aaa949a78f3c9df8c5a92028f7e4f
L V Ramana <lellalvramana@gmail.com>
2025-05-18 14:40:48 +0530

    shell practice

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -10,12 +10,30 @@ else
      echo " script is running by root user"
 fi
 
-dnf install mysql -y
+dnf installed list mysql
+if [$? -ne 0 ]
+then
+    echo "dnf mysql is not yet installed, installing now"
+    dnf install mysql -y
 
-if [ $? -eq 0 ]
-then  
-    echo "mysql installling successfully"
-else
+    if [ $? -eq 0 ]
+    then  
+    echo "mysql installed successfully"
+    else
     echo "mysql installtion falied "
     exit 1
+    fi
+
+else
+echo "mysql is already installed"
 fi
+
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then  
+#     echo "mysql installed successfully"
+# else
+#     echo "mysql installtion falied "
+#     exit 1
+# fi

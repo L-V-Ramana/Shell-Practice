cb07e276d55a7694a48bcf4d289f85c81d7e3c06
L V Ramana <lellalvramana@gmail.com>
2025-05-18 14:29:40 +0530

    shell practice

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -5,6 +5,17 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then 
       echo "error: access denied, please run the script with root access "
+      exit 1 #give exit code as 1 to 127 other than 0 because 0 exit code means previos command is success
 else
      echo " script is running by root user"
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then  
+    echo "mysql installling successfully"
+else
+    echo "mysql installtion falied "
+    exit 1
 fi

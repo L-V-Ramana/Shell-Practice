9711cc080da3bcdd5404bdb2c9cfac242abc012a
L V Ramana <lellalvramana@gmail.com>
2025-05-18 13:47:27 +0530

    shell practice

---
+++ 09-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+number=$1
+
+# -gt = greater than 
+#  -lt  = less than
+#  -eq = equal
+#  -ne = not equal
+
+if [ $number -lt 10]
+then 
+    echo "given $number is less than 10"
+else
+    echo "given $number is grester than 10"
+fi

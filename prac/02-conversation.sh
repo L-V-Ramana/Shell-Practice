2b2952a0286987b87b92259cb2ed7ae9ef5511d4
L V Ramana <lellalvramana@gmail.com>
2025-10-15 07:05:03 +0530

    practice

--- prac/02-conversation.sh
+++ prac/02-conversation.sh
@@ -45,7 +45,18 @@ else
         echo "$number is greater than 10"
     else
         echo "$number is equal to 10"
-    fi
+    fi  
+fi
+
+if [ ${number} -lt 10 ]
+then
+    echo "$number is less than 10"
+elif [ $number -gt 10 ]
+    then
+        echo "$number is greater than 10"
+    else
+        echo "$number is equal to 10"
+    fi  
 fi
 
 

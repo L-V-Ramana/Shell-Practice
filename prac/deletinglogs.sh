8b2ef30a991e373a233106292b7cc474bd2b0af7
L V Ramana <lellalvramana@gmail.com>
2025-10-20 17:13:29 +0530

    rooboshop

--- prac/deletinglogs.sh
+++ prac/deletinglogs.sh
@@ -1,9 +1,25 @@
 #/bin/bash
-
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+user=$(id -u)
 sourceDir="home/ec2-user/app-logs"
+
+
+
+if [$user -ne 0]
+then 
+    echo -e "$R Error $N login as root user"
+    exit 1
+else
+    echo "$G Success $N, root user"
+
+
 files=$(find $sourceDir -name "*.logs" -mtime+14)
 
-while [ ISF = read -r $files ]
+
+while [ ISF=read -r files ]
 do
     echo "deleting files as $files"
     rm -rf

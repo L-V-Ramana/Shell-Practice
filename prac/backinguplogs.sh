bd1431b7706143744f67e539c2873b5d6838645e
L V Ramana <lellalvramana@gmail.com>
2025-10-21 17:05:01 +0530

    backingup

---
+++ prac/backinguplogs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash 
+# logs backing up
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DATE=${3:-14}
+
+if [ $# -lt 2 ]
+then 
+    useage
+    exit 1
+
+    if [ ! -d $SOURCE_DIR ]
+    then
+        echo -e " $R Error $N No source directory $SOURCE_DIR"
+    fi
+
+    if [ ! -d $DEST_DIR  ]
+    then
+        echo -e "$R Error $N No destination directory $DEST_DIR"
+    fi
+fi
+
+files=$(find $SOURCE_DIR -name "*.log" -mtime +$DATE)
+
+if [ ! -z $files ]
+then 
+    timestamp=$(date+f-%h-%m-%s)
+    filename="$DEST_DIR/$timestamp.zip"
+    $(find $SOURCE_DIR -name "*.log" -mtime +$DATE)|zip @ $filename
+
+    if [ -f $filename ]
+    then 
+        echo -e "Succesfully files zipped"
+
+        while ISF = read -r file
+        do
+            echo -e "$G $file is being removed $N"
+            rm -rf $file
+        done >>>$files
+    else
+        echo -e "cration of zip is failed"
+    fi
+
+
+
+useage(){
+
+    echo -e " $R sh <script-name> <source-dir> <dest-dir> <days>(optional) $N "
+}

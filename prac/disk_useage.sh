09c1b1bbfaf192ba2987ef9c18a3f945cc6dc958
L V Ramana <lellalvramana@gmail.com>
2025-10-24 06:55:04 +0530

    backingup

--- prac/disk_useage.sh
+++ prac/disk_useage.sh
@@ -5,7 +5,7 @@ ip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4) #too get the priva
                                                                 #the server
 
 
-while IFS = read -r line
+while IFS= read -r line
 do 
    useage= $line|awk -f " " '{PRINT $6F}'|cut -d '%' f1
     partions= $line|awk -f " " '{PRINT $7F}'
